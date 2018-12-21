966218fd1635b0ef63c8e2a50f895a30a47a9ac4
kurodutch <jp_511@hotmail.es>
2018-12-20 22:30:25 -0300

    Se agrega arreglo penca

---
+++ Arreglo Basico/ProgramaPrueba/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrearGrid();
+            Console.ReadKey();
+        }
+
+        static void CrearGrid()
+        {
+            int ancho = 10;
+            int largo = 10;
+            int numero = 0;
+            int[,] cuadricula = new int[ancho, largo];
+            for (int x= 0; x < ancho; x++)
+            {
+                for (int y=0; y< largo; y++)
+                {
+                    cuadricula[x, y] = numero;
+                    Console.Write(cuadricula[x, y] + " " );
+                    numero++;
+
+                }
+                Console.WriteLine();
+
+
+            }
+        }
+
+
+
+
+    }
+           
+            
+            //goto Comienzo;
+            
+
+
+        
+    
+}
