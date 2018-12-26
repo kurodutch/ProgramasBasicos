0f33879812e8e71eade1f5770b3591130a201f84
kurodutch <jp_511@hotmail.es>
2018-12-25 22:39:06 -0300

    Agregado 2 programas más

---
+++ EnumBasico/InheritanceProgram/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProgram
+{
+    class MainClass
+    {
+
+        enum Raza {Akita, Boxer, Chihuahua, Cocker, Labrador, Pug};
+        //Clase Base
+        class Animal
+        {
+            public string nombre;
+            public byte edad;
+            public float felicidad;
+        }
+
+        //Clase Derivada
+        class Perro : Animal
+        {
+            public Raza raza;
+            public Perro(string _nombre, byte _edad, float _felicidad, Raza _raza)
+            {
+                nombre = _nombre;
+                edad = _edad;
+                felicidad = _felicidad;
+
+                raza = _raza;
+
+            }
+            public void Print() //imprime info de nuestro perro en la pantalla.
+            {
+                Console.WriteLine("nombre: " + nombre);
+                Console.WriteLine("edad: " + edad);
+                Console.WriteLine("felicidad: " + felicidad);
+                Console.WriteLine("Raza: " + raza);
+            }
+
+            
+
+
+
+        }
+
+        public static void Main( string[] args)
+        {
+            Perro Ronaldo = new Perro("Ronaldo", 14, 0.8f, Raza.Cocker);
+            Ronaldo.Print();
+            Perro Peggy = new Perro("Peggy", 2, 0.9f, Raza.Pug);
+            Peggy.Print();
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
---
+++ GetSetBasico/InheritanceProgram/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceProgram
+{
+    class MainClass
+    {
+        class Player
+        {
+            private int _salud = 100;
+            //public int salud = 100;
+            public int dañotomado;
+            public string nombre;
+            public int salud
+            {
+                get
+                {
+                    return _salud;
+                }
+                set
+                {
+                    if (value <= 0)
+                    {
+                        _salud = 0;
+                    } else if (value >= 100)
+                    {
+                        _salud = 100;
+                    }
+                    else
+                    {
+                        _salud = value;
+                    }
+
+
+
+                }
+
+            }
+            public Player()
+            {
+
+            }
+
+            public Player (int _salud, string _nombre){
+                salud = _salud;
+                nombre = _nombre;
+
+            }
+
+            public void Daño(int _daño)
+            {
+                _salud -=_daño;
+                dañotomado = _daño;
+            }   
+        }
+        public static void Main( string[] args)
+        {
+            Player Juan = new Player(100, "Juan");
+            Console.WriteLine("Nombre: " + Juan.nombre + " Salud: " + Juan.salud);
+            Player Tom = new Player();
+            Tom.Daño(47);
+            Console.WriteLine(Tom.salud);
+            Tom.salud -= 200;
+            Console.WriteLine(Tom.salud);
+            Tom.salud += 400;
+            Console.WriteLine(Tom.salud);
+            Console.WriteLine("El jugador tomó " + Tom.dañotomado+ " de daño, su salud actual es " + Tom.salud);
+            Console.ReadKey();
+
+
+
+
+
+        }
+
+    }
+
+}
